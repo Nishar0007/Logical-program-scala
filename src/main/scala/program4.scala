c1ffda16e11c02382c481c65d8d4283f60a37dff
NISHAR <abdulnizar899@gmail.com>
2023-11-05 12:53:29 +0530

    even or odd and divide value

---
+++ src/main/scala/program4.scala
@@ -0,0 +1,19 @@
+object program4 {
+    def main(args:Array[String]):Unit={
+    //Store a number in variable
+      val  n  = 100
+      println(n)
+
+      if(n>=100 && n<=1000){
+        if(n%2==0){
+          print(n%3)
+        }
+        else if(n%2==1){
+          print(n%2)
+        }
+      }
+      else{
+        print("invalid")
+      }
+  }
+}
