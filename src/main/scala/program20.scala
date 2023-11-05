131c95a539bca875072ed4b698e79cd6ebbf11ab
NISHAR <abdulnizar899@gmail.com>
2023-11-05 20:13:56 +0530

    A,B

---
+++ src/main/scala/program20.scala
@@ -0,0 +1,13 @@
+object program20 {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 1 to 100) {
+      if(i%2==1){
+        print("A"+",")
+      }
+      else{
+        print("B"+",")
+      }
+    }
+  }
+}
