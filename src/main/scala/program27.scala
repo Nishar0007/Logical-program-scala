5fe5c9c8e8b6f4bc0374bd67f0a0a0cf40573f70
NISHAR <abdulnizar899@gmail.com>
2023-11-06 13:28:06 +0530

    factorial of 3

---
+++ src/main/scala/program27.scala
@@ -0,0 +1,12 @@
+object program27 {
+  def main(args: Array[String]): Unit = {
+    for(i <- 1 to 23){
+      if(i%3==0){
+        print("factor of three"+" ")
+      }
+      else{
+        print(i+" ")
+      }
+    }
+  }
+}
