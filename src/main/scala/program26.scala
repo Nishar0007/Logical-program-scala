a400e9f5380f9f16615888d036cd77010dfad9a5
NISHAR <abdulnizar899@gmail.com>
2023-11-06 13:21:28 +0530

    print in 1,even

---
+++ src/main/scala/program26.scala
@@ -0,0 +1,14 @@
+object program26 {
+  def main(args: Array[String]): Unit = {
+    for(i <- 1 to 36){
+      if(i%2==0){
+        print("even"+" ")
+      }
+      else{
+        print(i+",")
+      }
+
+    }
+
+  }
+}
